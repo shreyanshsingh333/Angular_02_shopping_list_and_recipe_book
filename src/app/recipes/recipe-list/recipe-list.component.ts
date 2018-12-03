be456c01758512f48059fed9459b99050fffa3cc
shreyansh singh <shreyanshsingh333@gmail.com>
2018-12-04 00:47:49 +0530

    created components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is simply a test.',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_' +
+      'recipe_power_pork_dishes-604134.jpg!s'),
+    new Recipe('A Test Recipe',
+      'This is simply a test.',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_' +
+      'recipe_power_pork_dishes-604134.jpg!s'),
+    new Recipe('A Test Recipe',
+      'This is simply a test.',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_' +
+      'recipe_power_pork_dishes-604134.jpg!s')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
