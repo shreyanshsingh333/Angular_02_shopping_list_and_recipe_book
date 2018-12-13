52a66ba2e6c2f8b3305533b922f9da1888586c20
shreyansh singh <shreyanshsingh333@gmail.com>
2018-12-14 01:57:04 +0530

    passing data b/w sibling component using event and property binding(combined)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from '../../recipes.model';
 
 @Component({
@@ -8,9 +8,13 @@ import {Recipe} from '../../recipes.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 }
