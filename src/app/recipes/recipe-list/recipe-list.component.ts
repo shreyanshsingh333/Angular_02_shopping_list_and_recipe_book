46f29e53849b213177ff725ef37f452a3c6f23ed
shreyansh singh <shreyanshsingh333@gmail.com>
2018-12-14 00:06:50 +0530

    implemented component binding output and input

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,15 +8,15 @@ import {Recipe} from '../recipes.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
+    new Recipe('A Test Recipe 1',
       'This is simply a test.',
       'https://c.pxhere.com/photos/8b/0f/food_meat_' +
       'recipe_power_pork_dishes-604134.jpg!s'),
-    new Recipe('A Test Recipe',
+    new Recipe('A Test Recipe 2',
       'This is simply a test.',
       'https://c.pxhere.com/photos/8b/0f/food_meat_' +
       'recipe_power_pork_dishes-604134.jpg!s'),
-    new Recipe('A Test Recipe',
+    new Recipe('A Test Recipe 3',
       'This is simply a test.',
       'https://c.pxhere.com/photos/8b/0f/food_meat_' +
       'recipe_power_pork_dishes-604134.jpg!s')
